bc98edd6b881b6f4966763622c63731c6f4e5b79
Yifan <zhangyifan2017@outlook.com>
2022-06-12 01:19:18 +0200

    finish?

--- app/src/main/java/com/example/pf3/MainActivity.java
+++ app/src/main/java/com/example/pf3/MainActivity.java
@@ -96,18 +96,45 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     }
 
-    private void reset() {
-        for (int i = 0; i < 25; i++) {
+    private void reset() { // TODO
+        for (int i = 0; i < 8; i++) {
             toRoom1(i);
         }
-        for (int i = 25; i < 50; i++) {
+        for (int i = 8; i < 16; i++) {
             toRoom2(i);
         }
-        for (int i = 50; i < 75; i++) {
+        for (int i = 16; i < 24; i++) {
             toRoom3(i);
         }
-        for (int i = 75; i < 100; i++) {
+        for (int i = 24; i < 32; i++) {
             toRoom4(i);
+        }
+        for (int i = 32; i < 40; i++) {
+            toRoom5(i);
+        }
+        for (int i = 40; i < 48; i++) {
+            toRoom6(i);
+        }
+        for (int i = 48; i < 56; i++) {
+            toRoom7(i);
+        }
+        for (int i = 56; i < 64; i++) {
+            toRoom8(i);
+        }
+        for (int i = 64; i < 72; i++) {
+            toRoom9(i);
+        }
+        for (int i = 72; i < 79; i++) {
+            toRoom10(i);
+        }
+        for (int i = 79; i < 86; i++) {
+            toRoom11(i);
+        }
+        for (int i = 86; i < 93; i++) {
+            toRoom12(i);
+        }
+        for (int i = 93; i < 100; i++) {
+            toRoom13(i);
         }
     }
 
@@ -141,7 +168,7 @@ public class MainActivity extends Activity implements SensorEventListener {
             System.arraycopy(event.values, 0, accelerometerReading,
                     0, accelerometerReading.length);
             mov = (event.values[2] - 9.8) * (event.values[2] - 9.8);
-            //text.setText(String.valueOf((int)mov));
+            text.setText(String.valueOf((int)mov));
             if ((int)mov > 2) {
                 //move();
 
@@ -193,7 +220,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     public void relocate(int i) {
-        int[] count = {0,0,0,0}; //should be 13(15?)
+        int[] count = {0,0,0,0,0,0,0,0,0,0,0,0,0}; //should be 13
         for (int j = 0; j < 100; j++) { //j stands for room number
             if (inRoom1(j)) {
                 count[0]++;
@@ -203,20 +230,37 @@ public class MainActivity extends Activity implements SensorEventListener {
                 count[2]++;
             } else if (inRoom4(j)) {
                 count[3]++;
+            } else if (inRoom5(j)) {
+                count[4]++;
+            } else if (inRoom6(j)) {
+                count[5]++;
+            } else if (inRoom7(j)) {
+                count[6]++;
+            } else if (inRoom8(j)) {
+                count[7]++;
+            } else if (inRoom9(j)) {
+                count[8]++;
+            } else if (inRoom10(j)) {
+                count[9]++;
+            } else if (inRoom11(j)) {
+                count[10]++;
+            } else if (inRoom12(j)) {
+                count[11]++;
+            } else if (inRoom13(j)) {
+                count[12]++;
             }
         }
         int sum = 0;
-        for (int k = 0; k < 4; k++) {
+        for (int k = 0; k < 13; k++) {
             count[k] = count[k] * count[k];
             sum += count[k];
         }
         if (sum == 0 ){
-            Arrays.fill(count,25);
+            Arrays.fill(count,7);
             sum = 100;
         }
 
         int pos = new Random().nextInt(sum);
-        text.setText(String.valueOf(count[0]));
 
         if (pos < count[0]) {
             toRoom1(i);
@@ -227,70 +271,191 @@ public class MainActivity extends Activity implements SensorEventListener {
         if (pos >= count[0]+count[1] && pos < count[0]+count[1]+count[2]){
             toRoom3(i);
         }
-        if (pos >= count[0]+count[1]+count[2] && pos < sum) {
+        if (pos >= count[0]+count[1]+count[2] && pos < count[0]+count[1]+count[2]+count[3]) {
             toRoom4(i);
         }
+        if (pos >= count[0]+count[1]+count[2]+count[3] && pos < count[0]+count[1]+count[2]+count[3]+
+                count[4]) {
+            toRoom5(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4] && pos < count[0]+count[1]+count[2]+
+                count[3]+count[4]+count[5]) {
+            toRoom6(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4]+count[5] && pos < count[0]+count[1]+
+                count[2]+count[3]+count[4]+count[5]+count[6]) {
+            toRoom7(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6] && pos < count[0]+
+                count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]) {
+            toRoom8(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7] && pos <
+                count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]) {
+            toRoom9(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]
+                && pos < count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]+count[9]) {
+            toRoom10(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]+
+                count[9] && pos < count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]+count[9]+count[10]) {
+            toRoom11(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]+
+                count[9]+count[10] && pos < count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]+count[9]+count[10]+count[11]) {
+            toRoom12(i);
+        }
+        if (pos >= count[0]+count[1]+count[2]+count[3]+count[4]+count[5]+count[6]+count[7]+count[8]+
+                count[9]+count[10]+count[11] && pos < count[0]+count[1]+count[2]+count[3]+count[4]+
+                count[5]+count[6]+count[7]+count[8]+count[9]+count[10]+count[11]+count[12]) {
+            toRoom13(i);
+        }
     }
-
     private void toRoom1(int i ) {
-        int randomX = new Random().nextInt(540);
-        int randomY = new Random().nextInt(1033);
+        int randomX = new Random().nextInt(277) + 442;
+        int randomY = new Random().nextInt(86)+ 1601;
         dots[i] = new ShapeDrawable(new OvalShape());
         dots[i].getPaint().setColor(Color.BLUE);
         dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
         dots[i].draw(canvas);
     }
     private void toRoom2(int i ) {
-        int randomX = new Random().nextInt(540) + 540;
-        int randomY = new Random().nextInt(1033);
+        int randomX = new Random().nextInt(221) + 221;
+        int randomY = new Random().nextInt(254) + 1601;
         dots[i] = new ShapeDrawable(new OvalShape());
         dots[i].getPaint().setColor(Color.BLUE);
         dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
         dots[i].draw(canvas);
     }
     private void toRoom3(int i ) {
-        int randomX = new Random().nextInt(540);
-        int randomY = new Random().nextInt(1033) + 1033;
+        int randomX = new Random().nextInt(221);
+        int randomY = new Random().nextInt(254) + 1601;
         dots[i] = new ShapeDrawable(new OvalShape());
         dots[i].getPaint().setColor(Color.BLUE);
         dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
         dots[i].draw(canvas);
     }
     private void toRoom4(int i ) {
-        int randomX = new Random().nextInt(540) + 540;
-        int randomY = new Random().nextInt(1033) + 1033;
+        int randomX = new Random().nextInt(155) + 144;
+        int randomY = new Random().nextInt(337) + 1264;
         dots[i] = new ShapeDrawable(new OvalShape());
         dots[i].getPaint().setColor(Color.BLUE);
         dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
         dots[i].draw(canvas);
     }
-
-
+    private void toRoom5(int i ) {
+        int randomX = new Random().nextInt(155) + 144;
+        int randomY = new Random().nextInt(337) + 927;
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom6(int i ) {
+        int randomX = new Random().nextInt(155) + 144;
+        int randomY = new Random().nextInt(337) + 591;
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom7(int i ) {
+        int randomX = new Random().nextInt(155) + 144;
+        int randomY = new Random().nextInt(337) + 254;
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom8(int i ) {
+        int randomX = new Random().nextInt(143) + 299;
+        int randomY = new Random().nextInt(337) + 254;
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom9(int i ) {
+        int randomX = new Random().nextInt(221);
+        int randomY = new Random().nextInt(254);
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom10(int i ) {
+        int randomX = new Random().nextInt(221) + 221;
+        int randomY = new Random().nextInt(254);
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom11(int i ) {
+        int randomX = new Random().nextInt(277) + 442;
+        int randomY = new Random().nextInt(86) + 168;
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom12(int i ) {
+        int randomX = new Random().nextInt(110) + 719;
+        int randomY = new Random().nextInt(407) + 168;
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
+    private void toRoom13(int i ) {
+        int randomX = new Random().nextInt(303) + 299;
+        int randomY = new Random().nextInt(235) + 591;
+        dots[i] = new ShapeDrawable(new OvalShape());
+        dots[i].getPaint().setColor(Color.BLUE);
+        dots[i].setBounds(randomX-size, randomY-size, randomX+size, randomY+size);
+        dots[i].draw(canvas);
+    }
     public void drawRoom() {
 
         Paint paint = new Paint();
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(8);
+        paint.setStrokeWidth(5);
         paint.setAntiAlias(true);
-        canvas.drawLine(0, 0, 1080, 0, paint);
-        canvas.drawLine(0, 2066, 1080, 2066, paint);
-        canvas.drawLine(0, 0, 0, 2066, paint);
-        canvas.drawLine(1080, 0, 1080, 2066, paint);
+        //frame
+        //drawOneRoom(0,0,1080,2066, paint);
 
+        //rooms
+        drawOneRoom(442,1601, 719,1687, paint);
+        drawOneRoom(221, 1601,442, 1855, paint);
+        drawOneRoom(0,1601, 221,1855, paint);
+        drawOneRoom(114,1264, 299, 1601, paint);
+        drawOneRoom(114, 927, 299, 1264, paint);
+        drawOneRoom(114, 591, 299, 927, paint);
+        drawOneRoom(114, 254, 299, 591, paint);
+        drawOneRoom(299, 254, 442, 591, paint);
+        drawOneRoom(0, 0, 221, 254, paint);
+        drawOneRoom(221, 0, 442, 254, paint);
+        drawOneRoom(442, 168, 719, 254, paint);
+        drawOneRoom(719, 168, 829, 575, paint);
+        drawOneRoom(299,666, 602, 826, paint);
 
     }
-
+    public void drawOneRoom(int xStart, int yStart, int xEnd, int yEnd, Paint paint) {
+        canvas.drawLine(xStart, yStart, xEnd, yStart, paint);
+        canvas.drawLine(xEnd, yStart, xEnd, yEnd, paint);
+        canvas.drawLine(xStart, yEnd, xEnd, yEnd, paint);
+        canvas.drawLine(xStart, yStart, xStart, yEnd, paint);
+    }
     public boolean inRoom1(int i) {
         if (dots[i] == null) {
             return false;
         }
         Rect r = dots[i].getBounds();
         int left = r.left;
-        int right = r.right;
         int top = r.top;
-        int bottom = r.bottom;
-        if (left >= 0 && left <= 540 && top >= 0 && top <= 1033) {
+        if (left >= 442 && left <= 719 && top >= 1601 && top <= 1687) {
             return true;
         } else {
             return false;
@@ -302,10 +467,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
         Rect r = dots[i].getBounds();
         int left = r.left;
-        int right = r.right;
         int top = r.top;
-        int bottom = r.bottom;
-        if (left >= 540 && left <= 1080 && top >= 0 && top <= 1033) {
+        if (left >= 221 && left <= 442 && top >= 1601 && top <= 1855) {
             return true;
         } else {
             return false;
@@ -317,10 +480,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
         Rect r = dots[i].getBounds();
         int left = r.left;
-        int right = r.right;
         int top = r.top;
-        int bottom = r.bottom;
-        if (left >= 0 && left <= 540 && top >= 1033 && top <= 2066) {
+        if (left >= 0 && left <= 221 && top >= 1601 && top <= 1855) {
             return true;
         } else {
             return false;
@@ -332,10 +493,125 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
         Rect r = dots[i].getBounds();
         int left = r.left;
-        int right = r.right;
         int top = r.top;
-        int bottom = r.bottom;
-        if (left >= 540 && left <= 1080 && top >= 1033 && top <= 2066) {
+        if (left >= 114 && left <= 299 && top >= 1264 && top <= 1601) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom5(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 114 && left <= 299 && top >= 927 && top <= 1264) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom6(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 114 && left <= 299 && top >= 591 && top <= 927) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom7(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 114 && left <= 299 && top >= 254 && top <= 591) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom8(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 299 && left <= 442 && top >= 254 && top <= 591) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom9(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 0 && left <= 221 && top >= 0 && top <= 254) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom10(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 221 && left <= 442 && top >= 0 && top <= 254) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom11(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 442 && left <= 719 && top >= 168 && top <= 254) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom12(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 719 && left <= 829 && top >= 168 && top <= 575) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean inRoom13(int i) {
+        if (dots[i] == null) {
+            return false;
+        }
+        Rect r = dots[i].getBounds();
+        int left = r.left;
+        int top = r.top;
+        if (left >= 299 && left <= 602 && top >= 666 && top <= 826) {
             return true;
         } else {
             return false;
@@ -345,7 +621,16 @@ public class MainActivity extends Activity implements SensorEventListener {
         if (inRoom1(i) ||
             inRoom2(i) ||
             inRoom3(i) ||
-            inRoom4(i)) {
+            inRoom4(i) ||
+            inRoom5(i) ||
+            inRoom6(i) ||
+            inRoom7(i) ||
+            inRoom8(i) ||
+            inRoom9(i) ||
+            inRoom10(i) ||
+            inRoom11(i) ||
+            inRoom12(i) ||
+            inRoom13(i)) {
             return true;
         } else {
             return false;
